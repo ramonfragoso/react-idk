736e0d06ea63b455aafa9bc1fae17837c130efbe
ramonfragoso <jose.ramon.silva@ccc.ufcg.edu.br>
2018-07-07 16:33:10 -0300

    first commitmente

---
+++ iarl-react/src/components/pages/HomePage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+const HomePage = () => (
+	<div>
+		<h1>HOME PEIJ</h1>
+		<Link to="/login">Login</Link>
+	</div>
+)
+
+export default HomePage;
